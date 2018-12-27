5f3d1cd0b0c10893042768a307f2adcb423dbbb4
Raul Filimon <filimon35@gmail.com>
2018-12-27 17:15:31 +0200

    basic grid layout

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="header">
+        1
+      </div>
+      <div class="sidebar">
+        2
+      </div>
+      <div class="middle">
+        3
+      </div>
+    </div>
+  </body>
+</html>
